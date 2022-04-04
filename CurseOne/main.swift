f62f307d04767fd5d30c670089027b237a04ffd1
Konstantin Savchenko <konstantinsavchenkoclip@gmail.com>
2022-04-04 11:31:58 +0300

    clear project

--- CurseOne/main.swift
+++ CurseOne/main.swift
@@ -5,28 +5,4 @@
 //  Created by Константин Савченко on 31.03.2022.
 //
 
-import UIKit
-
-//1
-
-func number(x: Int) -> String {
-    if x % 2 == 0 {
-        return "Четное"
-    } else {
-        return "Нечетное"
-    }
-}
-
-print(number(x: 8))
-
-//2
-
-func number(x: Int) -> String {
-    if x % 3 == 0 {
-        return "Делится на 3 без остатка"
-    } else {
-        return "Не делится на 3 без остатка"
-    }
-}
-
-print(number(x: 12))
+import Foundation
