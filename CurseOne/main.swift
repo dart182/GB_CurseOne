5b3c654a8401a07a9ae45ad9ee7381db11189613
Konstantin Savchenko <konstantinsavchenkoclip@gmail.com>
2022-04-19 18:02:35 +0300

    HomeWork6 final

--- CurseOne/main.swift
+++ CurseOne/main.swift
@@ -9,11 +9,11 @@ import Foundation
 
 //MARK: -  1. Реализовать свой тип коллекции «очередь» (queue) c использованием дженериков
 ///Очередь
-struct GenericQueue<T> {
+class GenericQueue<T> {
     
-    var elements: [T] = []
+    var elements = [T]()
     
-    mutating func pushElement(_ element: T) {
+    func pushElement(_ element: T) {
         elements.append(element)
     }
     
@@ -23,7 +23,6 @@ struct GenericQueue<T> {
 }
 
 var listOfPeople = GenericQueue<String>()
-var listOfNumbers = GenericQueue<Int>()
 
 listOfPeople.pushElement("Artem")
 listOfPeople.pushElement("Boris")
@@ -31,19 +30,14 @@ listOfPeople.pushElement("Maga")
 listOfPeople.pushElement("Oleg")
 listOfPeople.pushElement("Grisha")
 
-listOfNumbers.pushElement(1)
-listOfNumbers.pushElement(2)
-listOfNumbers.pushElement(3)
-listOfNumbers.pushElement(4)
-listOfNumbers.pushElement(5)
-
-listOfNumbers.elements.filter { $0 % 2 == 0 }
-
-print(listOfNumbers.elements)
-
 print(listOfPeople.elements)
 
-listOfPeople.elements.forEach() {
+//MARK: - 2. Добавить ему несколько методов высшего порядка
+var listOfClanPeople = listOfPeople.elements.map({$0 + " [DEV]"})
+
+var listOfClanPeopleSorted = listOfClanPeople.sorted(by: {$0 < $1})
+
+listOfClanPeopleSorted.forEach() {
     print($0)
 }
 
